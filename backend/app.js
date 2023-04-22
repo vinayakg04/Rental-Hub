d26cc095b1c3fe924c2da8c9ba6240cec76cb3a6
Rajiv Kumawat <122862213+Rajivkumawat3@users.noreply.github.com>
2023-04-22 17:21:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use("/api/v1",payment)
 
 if (process.env.NODE_ENV) {
   //static folder add
-app.use(express.static(../frontend/build));
+app.use(express.static("../frontend/build"));
 app.get("*", function (req, res) {
   // res.sendFile(path.resolve('client', 'build' , 'index.html'));
   res.sendFile(path.resolve(__dirname , "../frontend/build/index.html"));
