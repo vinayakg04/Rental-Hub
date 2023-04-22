f59ce9eccb6a25175e2c09908a9ced7c01ca0adf
Rajiv Kumawat <122862213+Rajivkumawat3@users.noreply.github.com>
2023-04-22 16:33:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-
-dotenv.config({path:"backend/config/config.env"})
+if(process.env.NODE_ENV!=="PRODUCTION")
+{
+   dotenv.config({path:"backend/config/config.env"})
+}
 
 
 
