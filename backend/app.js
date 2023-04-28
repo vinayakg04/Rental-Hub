bcbf46dd5879df358021e1f8b20570b991431cc0
Rajiv Kumawat <122862213+Rajivkumawat3@users.noreply.github.com>
2023-04-28 15:45:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV) {
 app.use(express.static("../frontend/build"));
 app.get("/", function (req, res) {
   // res.sendFile(path.resolve('client', 'build' , 'index.html'));
-   res.sendFile(path.resolve(__dirname , "../frontend/build/index.html"));
+   res.send("hello");
    
 });
 }
