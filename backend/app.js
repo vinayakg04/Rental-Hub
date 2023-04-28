19f81059317c5f0f6624a28eadfe9d51eba33037
Rajiv <rajivkumawat28012002@gmail.com>
2023-04-28 17:08:21 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use("/api/v1",payment)
 
 app.use(express.static(path.join(__dirname,"../frontend/build")))
 app.get("*",(req,res)=>{
-     res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
 })
 // middleware for errors 
 app.use(errorMiddleware);
